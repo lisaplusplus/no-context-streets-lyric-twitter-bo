8a846f8ea49be4a283836f367427faf0ace7d6af
lisaplusplus <0lisa.burton@gmail.com>
2017-09-17 00:24:53 +0100

    Added filtered lyric list and filter script. Added update profile operation.

--- doTweet.py
+++ doTweet.py
@@ -1,3 +1,4 @@
+import argparse
 import tweepy
 import random, os, json
 
@@ -5,9 +6,16 @@ import logging
 SCRIPT_PARENT = os.path.dirname(os.path.abspath(__file__))
 logging.basicConfig(filename=SCRIPT_PARENT + '/originalTweetsMaterialBot.log',format='%(asctime)s %(message)s',level=logging.INFO)
 
-LYRIC_SOURCE_FILE= SCRIPT_PARENT + "/datasources/songLyrics.txt"
+LYRIC_SOURCE_FILE= SCRIPT_PARENT + "/datasources/filteredSongLyrics.txt"
 TWEET_RESERVOIR_FILE = SCRIPT_PARENT + "/tweetReservoir.txt"
 SECRETS_FILE= SCRIPT_PARENT + "/secrets.json"
+
+# Bot profile info
+BOT_NAME="NoCtxStreetsTweetBot"
+BOT_URL="http://spacekitcat.com"
+BOT_LOC="The Streets"
+BOT_VRS="v0.1"
+BOT_ABT="No context lyrics from The Streets. " + BOT_VRS
 
 # Generate the tweet 'resoviour' from songLyrics.txt
 #   generated in ./datasources/ (from the internet)
@@ -68,18 +76,42 @@ def popRandomReservoirEntry():
 
 # Main
 
+
 # Reset the reservoir when we run out of new things to tweet
 if isTweetReservoirDepleted():
   logging.info("Depleted reserviour detected. Resetting")
   initialiseLyricReservoir()
 
-
 # Load authentication keys
 with open(SECRETS_FILE) as secretsFileHandle:
   globals().update(json.load(secretsFileHandle))
 
+def parseargs():
+  parser = argparse.ArgumentParser(description='No context lyric bot.')
+  parser.add_argument('-t', '--dotweet', dest='do_tweet', action='store_true',
+                    default=True,
+                    help='Pop and Tweet next lyric.')
+  parser.add_argument('-u', '--updateprofile', dest='update_profile', action='store_true',
+                    default=False,
+                    help='Update the bots profile description without tweeting.')
+
+  return parser.parse_args()
+
+def dotweet(api):
+  logging.info("Running do_tweet operation.")
+  api.update_status(popRandomReservoirEntry());
+
+def updateprofile(api):
+  logging.info("Running update_profile operation")
+  api.update_profile(BOT_NAME, BOT_URL, BOT_LOC, BOT_ABT)
+  
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
-
 api = tweepy.API(auth)
-api.update_status(popRandomReservoirEntry());
+
+cli_args = parseargs()
+if cli_args.update_profile:
+  updateprofile(api)
+
+if cli_args.do_tweet:
+  dotweet(api)
